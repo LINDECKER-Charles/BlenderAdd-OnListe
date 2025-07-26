65b09916052586f3aff9538697499269da877ebe
LINDECKER Charles <charles.lindecker@outlook.fr>
2025-07-26 18:21:56 +0200

    Ajout de log au worker

--- app_blenderCollection/src/MessageHandler/DownloadZipMessageHandler.php
+++ app_blenderCollection/src/MessageHandler/DownloadZipMessageHandler.php
@@ -1,19 +1,23 @@
 <?php
 
-// src/MessageHandler/DownloadZipMessageHandler.php
 namespace App\MessageHandler;
 
 use App\Message\DownloadZipMessage;
 use App\Service\AddonDownloader;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class DownloadZipMessageHandler
 {
-    public function __construct(private AddonDownloader $addonDownloader) {}
+    public function __construct(
+        private readonly AddonDownloader $addonDownloader,
+        private readonly LoggerInterface $logger
+    ) {}
 
     public function __invoke(DownloadZipMessage $message): void
     {
+        $this->logger->info('Téléchargement du ZIP : ' . $message->zipName);
         $this->addonDownloader->downloadAndZipForce($message->urls, $message->zipName);
     }
 }
